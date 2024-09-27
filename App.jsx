ae2ad3341a619821e84dc503444edb3a88bb1bd4
pranulkbv28 <28bvpranaav@gmail.com>
2024-09-27 22:23:23 +0530

    inital commit

---
+++ App.jsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Main Page</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'red',
+  },
+});
